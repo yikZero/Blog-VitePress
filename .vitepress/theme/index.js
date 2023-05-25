065cf79177a46d00dfd3e65492051320e27a51fc
Zero <i@yikzero.com>
2023-05-25 13:40:36 +0800

    ✨ feat: 支持medium-zoom插件

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,10 +1,29 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-import Theme from 'vitepress/theme'
-import './style.css'
+import { h } from 'vue';
+import Theme from 'vitepress/theme';
+import './style.css';
+import { useRoute } from 'vitepress';
+import { onMounted, watch, nextTick } from 'vue';
+import mediumZoom from 'medium-zoom';
 
 export default {
   ...Theme,
+
+  setup() {
+    const route = useRoute();
+    const initZoom = () => {
+      // mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' });
+      new mediumZoom('.main img', { background: 'var(--vp-c-bg)' });
+    };
+    onMounted(() => {
+      initZoom();
+    });
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    );
+  },
+
   Layout: () => {
     return h(Theme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
@@ -12,5 +31,5 @@ export default {
   },
   enhanceApp({ app, router, siteData }) {
     // ...
-  }
+  },
 }
